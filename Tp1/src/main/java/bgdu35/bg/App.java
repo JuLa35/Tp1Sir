314a485956ed72d3e4fea61f540c920a9008094c
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2019-01-08 10:31:03 +0100

    Add files via upload

---
+++ Tp1/src/main/java/bgdu35/bg/App.java
@@ -0,0 +1,14 @@
+package bgdu35.bg;
+
+/**Hello world!
+ * @author bgdu35
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	new JXBusyLabelTest();
+        System.out.println( "Hello World!" );
+    }
+}
